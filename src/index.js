80a1983c8d1b1fdd2e38293973727cac12d259d9
catshirt <caden@catshirt.net>
2013-02-27 03:14:25 -0500

    adding cors support

--- src/index.js
+++ src/index.js
@@ -10,6 +10,14 @@ define([
     // make sure we can decode POST body data
     app.use(express.bodyParser());
 
+    // add cors headers
+    app.use(function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', req.headers.origin);
+      res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+      next();
+    });
+
     // setup the routes for each model or collection
     _(resources).each(function(Resource) {
       var path = Resource.prototype.urlRoot || Resource.prototype.url,
@@ -18,6 +26,16 @@ define([
         coll_ep = path;
 
       if (is_model) {
+
+        // OPTIONS /:urlRoot/:id
+        app.options(coll_ep, function(req, res) {
+          res.send(200);
+        });
+
+        // OPTIONS /:urlRoot/:id
+        app.options(model_ep, function(req, res) {
+          res.send(200);
+        });
 
         // POST /:urlRoot
         app.post(coll_ep, function(req, res) {
@@ -105,6 +123,11 @@ define([
         });
 
       } else {
+
+        // OPTIONS /:url
+        app.options(coll_ep, function(req, res) {
+          res.send(200);
+        });
 
         // GET /:url
         app.get(coll_ep, function(req, res) {
